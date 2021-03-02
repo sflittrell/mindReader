b075e31ad506fbc9f96ce2c61801b16239905454
sflittrell <sflittrell@gmail.com>
2021-03-02 18:40:12 -0500

    fixed the issue with the special character not being updated after the reset button had been pressed

--- js/main.js
+++ js/main.js
@@ -25,6 +25,12 @@ function addSym() {
     return numSymList;
 }
 
+function symReset() {
+    state.mainText[4] = numSymList;
+    state.mainText[5] = specialSym;
+    state.exampleText[5] = 'Your symbol is: ' + specialSym;
+}
+
 function next() {
     currentState += 1;
     render();
@@ -39,30 +45,36 @@ function reset() {
     currentState = 0;
     specSym();
     addSym();
+    symReset();
     render();
 }
 
-addSym();
-specSym();
+function backButton() {
+    if (currentState === 0) {
+        next();
+    } else if (currentState > 0 && currentState < 5) {
+        back();
+    } else {
+        reset();
+    }
+}
+
+// addSym();
+// specSym();
 
 
 let state = {
     mainText: ['I can read your mind', 'Pick a number from 01 - 99', 'Add both digits together to get a new number', 'Subtract your new number from the original numbers', numSymList, specialSym],
     nextButton: ['invisible', 'visible', 'visible', 'visible', 'visible', 'invisible'],
     exampleText: ['', 'when you have your number click next', 'Ex: 14 is 1 + 4 = 5 click next to proceed', 'Ex: 14 - 5 = 9 click next to proceed', 'Scroll to find your new Number. Note the symbol beside the number', 'Your symbol is: ' + specialSym],
-    goButton: ['visible', 'invisible', 'invisible', 'invisible', 'invisible', 'invisible'],
-    backButton: [ 'invisible', 'visible', 'visible', 'visible', 'visible', 'invisible'],
-    resetButton: ['invisible', 'invisible', 'invisible', 'invisible', 'invisible', 'visible']
-
+    backButton: ['GO', 'Back', 'Back', 'Back', 'Back', 'Reset'],
 }
 
 function render() {
     document.getElementById('header').innerHTML = state.mainText[currentState];
-    document.getElementById('nextBtn').setAttribute('class', state.nextButton[currentState]);
+    document.getElementById('cloak').setAttribute('class', state.nextButton[currentState]);
     document.getElementById('exampleText').innerHTML = state.exampleText[currentState];
-    document.getElementById('goBtn').setAttribute('class', state.goButton[currentState]);
-    document.getElementById('backBtn').setAttribute('class', state.backButton[currentState]);
-    document.getElementById('resetBtn').setAttribute('class', state.resetButton[currentState]);
+    document.getElementById('backBtn').innerHTML = state.backButton[currentState];
 }
 
 reset();
